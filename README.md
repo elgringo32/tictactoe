ebe0cde1c9a6b653300517b95f02e6285ff6d5aa
elgringo32 <bknopke@gmail.com>
2021-01-12 19:17:35 -0800

    add ability to add x/o pay player

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tictactoe
+
+vh = 1% of view port or size of browser
+
+factory function
+built for Player objects
+
+module built for game play and gameboard create
--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-const gameBoard = document.getElementById('game-board')
-    var i;
-    for (i=0; i<9; i++) {
-        const gridBlock = document.createElement('div');
-        gridBlock.classList.add('grid-block');
-        gameBoard.appendChild(gridBlock);
-    }    
-
-
-
 //Player Factory
 const Player = (name, position) => {
 	const getName = () => name;
 	const getPosition = () => position;
-	const playTurn  = () => {
+	function playTurn() {
         if (position === 1) {
-            console.log('x');
+            return 'x';
         }
         if (position === 2) {
-            console.log('o');
+            return 'o';
         }
     }
-    return {getName, getPosition, playTurn}
+    return {
+        getName, 
+        getPosition, 
+        playTurn
+    }
 }
 
 const player1 = Player('brian', 1);
 const player2 = Player('cruz', 2);
 
-player1.playTurn();
+let currentPlayer = player1;
+
+function switchPlayer() {
+    if (currentPlayer === player1) {
+        currentPlayer = player2;
+    }
+    else if (currentPlayer === player2) {
+        currentPlayer = player1;
+    }; 
+    return currentPlayer;
+}
+
 
 //Board Module
-const gameBord = (() => {
+function gameBoardBuilder(){
+    const gameBoard = document.getElementById('game-board');
+        var i;
+        for (i=0; i<9; i++) {
+            const gridBlock = document.createElement('div');
+            gridBlock.classList.add('grid-block');
+            gridBlock.setAttribute('data-cell-id', i+1);
+            gameBoard.appendChild(gridBlock);
 
-})();
+        };
+    let gameBoardCells = document.querySelectorAll('.grid-block');
+    gameBoardCells.forEach(cell => cell.addEventListener('click', markCell));    
+    }
+
+
+function markCell() {
+    if (!this.innerText) {
+        let mark = currentPlayer.playTurn();
+        this.innerText = mark;
+        switchPlayer();
+    }
+    else {
+        alert('square is already taken :(')
+    }
+    
+}
+
+gameBoardBuilder();
+
